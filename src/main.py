b2476d8e59dd224d6a013981bc33f6dc11ee414e
Sasha Khinevich <hinevics@gmail.com>
2023-06-27 00:47:12 +0300

    ✨feat: create min fuction

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 import logging
 
-from telegram.ext import ApplicationBuilder, CommandHandler
+from telegram import Update
+from telegram.ext import (
+    ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler,
+    filters,
+)
 
 from config import TG_TOKEN
+from check_code import check
 
-# Настройка журналирования
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO)
@@ -12,15 +17,37 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def start(update, context):
-    return context.bot.send_message(chat_id=update.effective_chat.id, text="Привет! Я бот Telegram.")
+def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    return context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Привет! Я бот Telegram. Отправь мне сой код!")
+
+
+# def checking(update: Update, context: ContextTypes.DEFAULT_TYPE):
+#     code = ' '.join(context.args)
+#     index = check(code)
+#     return context.bot.send_message(
+#         chat_id=update.effective_chat.id, text=f'score: {index}'
+#     )
+
+
+def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    code = update.message.text
+    index = check(code)
+    return context.bot.send_message(
+        chat_id=update.effective_chat.id, text=f'score: {index}')
 
 
 def main():
     app = ApplicationBuilder().token(TG_TOKEN).build()
     start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(filters.TEXT, echo)
+    # checking_handler = CommandHandler('checking', checking)
     app.add_handler(start_handler)
+    app.add_handler(echo_handler)
+    # app.add_handler(checking_handler)
     app.run_polling()
+
 
 if __name__ == '__main__':
     main()
