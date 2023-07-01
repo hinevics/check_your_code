3e1d91d1df6ffd210601c5fcb6380a0a86105eb7
Sasha Khinevich <hinevics@gmail.com>
2023-07-01 13:54:11 +0300

    ✨feat: add logger

--- src/config.py
+++ src/config.py
@@ -5,3 +5,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TG_TOKEN = os.getenv('TG_TOKEN')
+PATH_DATA = os.getenv('PATH_DATA')
--- src/loader.py
+++ src/loader.py
@@ -1,7 +1,14 @@
 # loading data
 
 import pandas as pd
+import pathlib
+
+import logger
 
 
-def load_data(path: str) -> pd.DataFrame:
-    return pd.read_csv(path)
+def load_data(path: pathlib.PosixPath) -> pd.DataFrame:
+    # Функция для простой подгрудки документа
+    if path.exists():
+        return pd.read_csv(path)
+    logger.error_logger('File not Exists!')
+    raise FileExistsError('Файла нет!')
