ba965f7d7d73e5740e143f652a2d271f20873afe
Sasha Khinevich <hinevics@gmail.com>
2023-07-09 18:21:17 +0300

    ✨feat: crete base fuction for tg-bot

--- src/check_code.py
+++ src/check_code.py
@@ -1,8 +1,2 @@
-import random
-
-
-def check(code: str) -> int:
-    max_val = len(code)
-    min_val = random.random()
-    x = random.randint(a=1, b=11)
-    return (x - min_val) / (max_val - min_val)
+def check(code1: str, code2: str) -> int:
+    return len(code1) / len(code2)
