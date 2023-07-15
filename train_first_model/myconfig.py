9462c1683769c82aee2fc48b1aab3417cd2496a8
Sasha Khinevich <hinevics@gmail.com>
2023-07-15 18:26:52 +0300

    ✨feat: add folder for train first model

---
+++ train_first_model/myconfig.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+TG_TOKEN = os.getenv('TG_TOKEN')
+PATH_DATA = os.getenv('PATH_DATA')
+PATH_MODELS = os.getenv('PATH_MODELS')
+LOG_PATH = os.getenv('LOG_PATH')
+LOG_ERROR_PATH = os.getenv('LOG_ERROR_PATH')
+PATH_TEMP = os.getenv('PATH_TEMP')
